fc89b811a444a3d1702763a409faa20cb5647d63
huntc2005 <huantc.23it@vku.udn.vn>
2025-07-18 20:15:55 +0700

    🛠️ Sửa lại logic nút Không + gif cute

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,16 @@ function App() {
       <div className="relative w-full h-full flex justify-center">
         <img
           src="https://iili.io/FXy52ZN.png"
-          className="w-[90vh] h-auto absolute z-0 left-0 top-14"
+          className="w-[90vh] h-auto absolute z-0 left-0 top-9"
           alt=""
         />
         <img
           src="https://iili.io/Fh9dtXR.png"
-          className="w-[90vh] h-auto absolute z-0 right-0 top-14"
+          className="w-[90vh] h-auto absolute z-0 right-0  top-96 xl:top-9"
           alt=""
         />
         {!accpeted ? (
-          <div className="w-[80vh] h-[60vh] bg-sky-100 border-4 border-sky-950 rounded-3xl shadow-2xl flex flex-col items-center py-5 gap-5 absolute top-36 ">
+          <div className="w-[50vh] h-[60vh]  xl:w-[80vh] xl:h-[60vh] bg-sky-100 border-4 border-sky-950 rounded-3xl shadow-2xl flex flex-col items-center py-5 gap-5 absolute top-[150px] xl:top-[9vw] ">
             <h1 className="text-blue-950 font-bold text-[18px] lg:text-[19px] xl:text-[22px]">
               Em có thích anh khongggg
             </h1>
@@ -65,7 +65,7 @@ function App() {
           <motion.div
             initial={{ scale: 0 }}
             animate={{ scale: 1 }}
-            className="text-center p-6 rounded-xl shadow-xl bg-gray-300 mt-8"
+            className="text-center p-6 rounded-xl shadow-xl bg-gray-300 mt-[20vh] xl:mt-8 z-10 flex flex-col justify-center items-center"
           >
             <img src="https://iili.io/FhHO9Rf.jpg"
             className="w-[50vh] h-auto" alt="" />
