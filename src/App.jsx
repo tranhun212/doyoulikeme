91d43423710d0878a9bd72b2ddb93cfe09043d67
tranhun212 <hun212205@gmail.com>
2026-03-14 17:13:08 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="w-full h-screen flex items-center justify-center bg-pink-100">
-
+      
       {!openLetter ? (
         <motion.button
           onClick={handleOpenLetter}
@@ -20,30 +20,33 @@ function App() {
           whileTap={{ scale: 0.9 }}
           className="px-8 py-4 bg-pink-500 text-white text-xl font-bold rounded-xl shadow-lg"
         >
-          Đọc thư 
+          Đọc thư
         </motion.button>
       ) : (
-      <motion.div
-  initial={{ opacity: 0 }}
-  animate={{ opacity: 1 }}
-  transition={{ duration: 1 }}
-  className="flex flex-col items-center gap-4"
->
-         <a
-    href={image1}
-    download="buc-thu.png"
-    className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition"
-  >
-    Download ảnh 📥
-  </a>
-  <img
-    src={image1}
-    className="w-[60vh] rounded-xl shadow-xl"
-    alt="Thu"
-  />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="flex flex-col items-center gap-4"
+        >
+          <img
+            src={image1}
+            className="w-[60vh] rounded-xl shadow-xl"
+            alt="Thu"
+          />
 
- 
-</motion.div>
+          <a
+            href={image1}
+            download="buc-thu.png"
+            className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition"
+          >
+            Download ảnh 📥
+          </a>
+
+        </motion.div>
+      )}
+
+    </div>
   );
 }
 
